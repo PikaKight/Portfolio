2b3ce16c6d9fc8dab1e4d8c0c3029419b3165032
PikaKight <tmmarcus73@gmail.com>
2024-10-08 17:36:50 -0400

    getting started

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      
+      <div className={styles.menu}>
+        <img className={styles.logo} src='../assets/MTXX.jpg' alt="Image of Marcus Tuen Muk"/>
+      </div>
+
+      
+    </div>
+  );
+}
