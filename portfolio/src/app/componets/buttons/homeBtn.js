dff22210e6591da387cef4bcb3463c8b6b03fe72
PikaKight <tmmarcus73@gmail.com>
2024-12-12 14:40:40 -0500

    getting the portfolio done

--- portfolio/src/app/componets/buttons/homeBtn.js
+++ portfolio/src/app/componets/buttons/homeBtn.js
@@ -1,7 +1,13 @@
+import styles from "../../page.module.css";
+
+
 const HomeBtnComponent = () => {
     return (
-        <div>
-
+        <div className={styles.button}>
+            <button 
+                className={styles.button}
+                onClick={console.log("hi")}
+            >Home</button>
         </div>
     )
 }
